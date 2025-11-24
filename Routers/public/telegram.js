e417227d8100394019c667763dd85c71cb6c1283
Nguyễn Thanh Long <81322103+longnt267@users.noreply.github.com>
2025-11-24 22:57:04 +0700

    post

--- Routers/public/telegram.js
+++ Routers/public/telegram.js
@@ -2,5 +2,5 @@ import express from "express";
 import { telegram, telegramSummary } from "../../Controllers/telegram.js";
 const telegramRoutes = express.Router();
 telegramRoutes.get("/", telegram);
-telegramRoutes.get("/summary", telegramSummary);
+telegramRoutes.post("/summary", telegramSummary);
 export { telegramRoutes };

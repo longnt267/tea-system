3e88a6da9e855e1c8d8f18d1d0da78edf5f21f90
Thanh Long <long.nt2607@gmail.com>
2025-11-25 11:27:09 +0700

    updatge key

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import publicRoutes from "./Routers/publicRoutes.js";
+import { dbMiddleware } from "./middleware/database.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors("*"));
-
+app.use(dbMiddleware);
 app.use("/api", publicRoutes);
 app.get("/", (req, res) => {
   res.send("Welcome to the Chat App API");
